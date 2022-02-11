4034d2a80ecab0987333b6c8881f36f2f183171e
salam <salamibraheem82@gmail.com>
2022-02-10 21:19:15 -0600

    updated env file

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
 		features="./src/test/resources/features",
 		glue="step_definition",
 		dryRun=false,
-		tags="@TradeJournalLogOut" 
+		tags="@SmokeTests" 
 		)
 public class TestRunner { 
  
